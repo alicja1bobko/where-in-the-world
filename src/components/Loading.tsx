8d124291bda09aa114400f2ec21ba2b36d552025
alicja1bobko <alicja1bobko@users.noreply.github.com>
2022-10-05 10:21:23 +0000

    deploy: 49b442303eac2e86fc44a3b46618e3a2a9814e5d

--- src/components/Loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin motion-reduce:animate-spin border-black dark:border-violet-400"></div>
-    </div>
-  );
-}
--- src/helpers/setTitle.tsx
+++
@@ -1,4 +0,0 @@
-export function setTitle(title: string, flag: string) {
-  document.title = title;
-  document.getElementById("favicon")?.setAttribute("href", flag);
-}
